af3c3619d7e80a534737d9465efda1cd71d155a1
Gabriel Franke <bannedboyinlife@gmail.com>
2021-08-17 15:20:22 -0300

    new folder for a bigger project

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
-// our-domain.com/
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/O2Z66UWrZH8zcejxopwWxhGu.png",
+    address: "Some address",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/O2Z66UWrZH8zcejxopwWxhGu.png",
+    address: "Some address",
+    description: "This is a second meetup!",
+  },
+];
 
 function HomePage() {
-  return <h1>The Home Page</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
