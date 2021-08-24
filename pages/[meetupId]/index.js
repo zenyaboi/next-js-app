6f66a671ae87a707dac86031da0958cae855e607
Gabriel Franke <bannedboyinlife@gmail.com>
2021-08-24 14:51:11 -0300

    2 new files and new index meetupid code

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://image.api.playstation.com/vulcan/img/rnd/202010/2614/O2Z66UWrZH8zcejxopwWxhGu.png"
+      title="First Meetup"
+      address="Some Street"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
