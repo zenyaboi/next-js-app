02a605bce2f33fd607cc853727445ba1deb3416a
Gabriel Franke <bannedboyinlife@gmail.com>
2021-08-30 14:50:17 -0300

    new code for index.js file

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,16 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    }
+  };
 }
 
 export default HomePage;
