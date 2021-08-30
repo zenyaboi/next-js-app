28accddcea3e53f7ee2f5482c0c8393def9eb458
Gabriel Franke <bannedboyinlife@gmail.com>
2021-08-30 15:20:15 -0300

    new code for index.js

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,21 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }  
+
+/* ----> This is another way of doing the getStaticProps(), but getServerSideProps() is used for when the data constantly changes.
+Since this project does not have data that changes every time, it's basically worthless using this method, but it's good to know that this function exists
+because it's useful depending on what projet you are developing  <---- */
+
 export async function getStaticProps() {
   return {
     props: {
